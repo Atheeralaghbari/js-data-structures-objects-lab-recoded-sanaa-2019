a764b55d4c705dbbc1544fb4662c4cf998e63418
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-04 20:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 // Write your solution in this file!
 
- function updateDriverWithKeyAndValue(dirver,key,value)
-{
-    let driver1=Object.assign({},dirver,{[key]:value})
-  return dirver1;
-}
-updateDriverWithKeyAndValue({name:"Ahmed",age:26}, "name", "Atheer");
+const driver={
+    name: "taghreed",
+    age: 26
+};
 
-function destructivelyUpdateDriverWithKeyAndValue(dirver,key,value){
-   let dirver4=dirver
-  dirver4[key]=value;
-  return dirver4;
+function updateDriverWithKeyAndValue(driver, key, value){
+    let driver2= Object.assign({}, driver, { [key]: value });
+    console.log(driver2);
+    return driver2;
+    
 }
-function deleteFromDriverByKey(dirver,key){
-  let dirver2 = Object.assign({}, dirver);
-  delete dirver2[key];
-   return dirver2;
+
+updateDriverWithKeyAndValue(driver, "name", "Reham");
+//console.log(updateDriverWithKeyAndValue(drivers, "name", "Reham"))
+console.log(driver)
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    let driver3= driver;
+    driver3[key]=value;
+    console.log(driver3);
+    return driver3;
 }
-function destructivelyDeleteFromDriverByKey(dirver,key){
- let dirver3=dirver;
- delete dirver3[key];
- return dirver3;
- 
+destructivelyUpdateDriverWithKeyAndValue(driver, "age", "40");
+console.log(driver)
+
+function deleteFromDriverByKey(driver, key){
+    let driver4 = Object.assign({},driver);
+    delete driver4[key];
+    console.log(driver4);
+    return driver4;
 }
+deleteFromDriverByKey(driver, "age")
+console.log(driver)
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    let driver5=driver;
+    delete driver5[key];
+    console.log(driver5);
+    return driver5;
+}
+destructivelyDeleteFromDriverByKey(driver, "name")
+console.log(driver)
