7d0821e5d326a6d30d3070261ff9a1e77a9ace1f
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-04 20:34:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function deleteFromDriverByKey(obj,key){
   const newObj = Object.assign({}, obj);
   delete newObj[key];
 }
-function destructivelyDeleteFromDriverByKey(d,key){
+function destructivelyDeleteFromDriverByKey(dr,key){
  let newObj=obj;
  delete newObj[key];
 }
